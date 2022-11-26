9ed144233c124f3851d31f5a4e930d4fe6df6c87
blazhkevych.v <blazhkevych.v@gmail.com>
2022-11-26 13:56:59 +0200

    s

--- task/Screen.cs
+++ task/Screen.cs
@@ -9,79 +9,79 @@ namespace task
     // Воспомогательный класс для отрисовки графики в консоли.
     internal class Screen
     {
-        public static readonly byte _TRACK_WIDTH = 72;
-        public static readonly byte _TRACK_HEIGHT = 1;
-        private static readonly byte _LEFT_OFFSET = 2;
-        private static readonly byte _TOP_OFFSET = 2;
+        public static readonly int _TRACK_WIDTH = 72;
+        public static readonly int _TRACK_HEIGHT = 1;
+        private static readonly int _LEFT_OFFSET = 2;
+        private static readonly int _TOP_OFFSET = 2;
 
         // Нарисовать гоночную дорожку для машины
-        public static void DrawTrack(byte nTrack)
+        public static void DrawTrack(int nTrack)
         {
-            byte top = (byte)(((nTrack - 1) * _TRACK_HEIGHT) + _TOP_OFFSET);
-            SetCursorPosition(_LEFT_OFFSET, top);
+            int top = (int)(((nTrack - 1) * _TRACK_HEIGHT) + _TOP_OFFSET);
+            Console.SetCursorPosition(_LEFT_OFFSET, top);
 
-            Write('|');
-            for (byte i = 0, len = (byte)(_TRACK_WIDTH - 3); i < len; ++i)
-                Write('_');
+            Console.WriteLine('|');
+            for (int i = 0, len = (int)(_TRACK_WIDTH - 3); i < len; ++i)
+                Console.WriteLine('_');
 
             if ((nTrack & 1) == 0)
-                Write("|# ");
+                Console.WriteLine("|# ");
             else
-                Write("| #");
+                Console.WriteLine("| #");
         }
 
         // Нарисовать машину на заданной дорожке
-        public static void DrawCar(Car car, byte nTrack)
+        public static void DrawCar(Car car, int nTrack)
         {
             // вычислить позицию машини на экране
-            ushort traveled = (ushort)(car.TraveledDistance / RacingGame.GridSize);
-            byte left = (byte)(_LEFT_OFFSET + traveled);
-            byte top = (byte)(((nTrack - 1) * _TRACK_HEIGHT) + _TOP_OFFSET);
+            int traveled = (int)(car.TraveledDistance / RacingGame.GridSize);
+            int left = (int)(_LEFT_OFFSET + traveled);
+            int top = (int)(((nTrack - 1) * _TRACK_HEIGHT) + _TOP_OFFSET);
 
-            SetCursorPosition(left, top);
-            Write(car.Graphics);
+            Console.SetCursorPosition(left, top);
+            Console.WriteLine(car.Graphics);
         }
 
         // Нарисовать информацию о гонке
         public static void DrawRaceInfo(List<Car> cars)
         {
-            byte top = (byte)(WindowHeight - _TOP_OFFSET - 1);
+            int top = (int)(Console.WindowHeight - _TOP_OFFSET - 1);
             top -= RacingGame.NumberOfCars;
 
             List<Car> sorted = cars.OrderBy(x => x.TraveledDistance).ToList();
             sorted.Reverse();
 
-            SetCursorPosition(_LEFT_OFFSET, top);
-            Write("/-------------------------------------------------\\");
-            SetCursorPosition(_LEFT_OFFSET, CursorTop + 1);
-            Write("|{0, -4}| {1, -16} | {2, -12} | {3, -8} |",
+            Console.SetCursorPosition(_LEFT_OFFSET, top);
+            Console.WriteLine("/-------------------------------------------------\\");
+            Console.SetCursorPosition(_LEFT_OFFSET, Console.CursorTop + 1);
+            Console.WriteLine("|{0, -4}| {1, -16} | {2, -12} | {3, -8} |",
                     "RANK", "CAR NAME", "CAR MODEL", "SPEED");
-            SetCursorPosition(_LEFT_OFFSET, CursorTop + 1);
+            Console.SetCursorPosition(_LEFT_OFFSET, Console.CursorTop + 1);
 
-            for (byte i = 0; i < RacingGame.NumberOfCars; ++i)
+            for (int i = 0; i < RacingGame.NumberOfCars; ++i)
             {
                 string fmt = String.Format(
                     "| №{0, -1} | {1, -16} | {2, -12} | {3, -3} km/h |",
                     i + 1, sorted[i].Name, sorted[i].Model, sorted[i].CurrentSpeed
                 );
 
-                Write(fmt);
-                SetCursorPosition(_LEFT_OFFSET, CursorTop + 1);
+                Console.WriteLine(fmt);
+                Console.SetCursorPosition(_LEFT_OFFSET, Console.CursorTop + 1);
             }
 
-            Write("\\-------------------------------------------------/");
+            Console.WriteLine("\\-------------------------------------------------/");
         }
 
         // Нарисовать произовольное сообщение на экране
         public static void DrawMessage(string msg)
         {
-            byte top = (byte)((RacingGame.NumberOfCars * _TRACK_HEIGHT) + _TOP_OFFSET);
-            SetCursorPosition(_LEFT_OFFSET, top + 2);
-            for (byte i = 0; i < _TRACK_WIDTH; ++i)
-                Write(' ');
-            SetCursorPosition(_LEFT_OFFSET, top + 2);
+            int top = (int)((RacingGame.NumberOfCars * _TRACK_HEIGHT) + _TOP_OFFSET);
+            Console.SetCursorPosition(_LEFT_OFFSET, top + 2);
+            for (int i = 0; i < _TRACK_WIDTH; ++i)
+                Console.WriteLine(' ');
+            Console.SetCursorPosition(_LEFT_OFFSET, top + 2);
 
-            Write($"Latest Info: {msg}");
+            Console.WriteLine($"Latest Info: {msg}");
         }
     }
 }
